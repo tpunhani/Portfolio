69e11ae86e242086d39a0e174855f763ac31ec82
Tarun Punhani <tpunhani@gmail.com>
2022-06-06 11:15:14 -0500

    Updates

---
+++ precache-manifest.9e43ea4f09e27698406b87a92bd056e9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823a0c13bdce9447de9677db97458860",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5340d83689c3be7246e3",
+    "url": "/Portfolio/static/css/3.d8506a19.chunk.css"
+  },
+  {
+    "revision": "1d1de558508db65e25dd",
+    "url": "/Portfolio/static/css/4.7fa851af.chunk.css"
+  },
+  {
+    "revision": "970cbd1cd0368bfc6bd5",
+    "url": "/Portfolio/static/css/main.3d9fce88.chunk.css"
+  },
+  {
+    "revision": "df9826b89ae3b611c3b5",
+    "url": "/Portfolio/static/js/2.952dd38b.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/Portfolio/static/js/2.952dd38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5340d83689c3be7246e3",
+    "url": "/Portfolio/static/js/3.696166cb.chunk.js"
+  },
+  {
+    "revision": "1d1de558508db65e25dd",
+    "url": "/Portfolio/static/js/4.36eec2a3.chunk.js"
+  },
+  {
+    "revision": "970cbd1cd0368bfc6bd5",
+    "url": "/Portfolio/static/js/main.9ee980f3.chunk.js"
+  },
+  {
+    "revision": "7b2e9e413624b6bf2174",
+    "url": "/Portfolio/static/js/runtime-main.4b6356de.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/Portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "032b663806b874c205dc1f8b2fdb2405",
+    "url": "/Portfolio/static/media/datingLogo.032b6638.jpeg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/Portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "133d2222c72435a87a134cb42a2d6def",
+    "url": "/Portfolio/static/media/niec-logo.133d2222.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/Portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "e1c7eb25508a427b4dfb1a8cc06427ca",
+    "url": "/Portfolio/static/media/runnybunny.e1c7eb25.jpeg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "81ef081abef4135bc93f68fd2ecc9b52",
+    "url": "/Portfolio/static/media/tarun1.81ef081a.png"
+  },
+  {
+    "revision": "eb750d735b526af89dbcb0cb10018a87",
+    "url": "/Portfolio/static/media/timesLogo.eb750d73.png"
+  },
+  {
+    "revision": "1d5ae2bac7165848fd6ba30e4c3bae27",
+    "url": "/Portfolio/static/media/utd-logo.1d5ae2ba.png"
+  }
+]);
